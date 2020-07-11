143df583596da6f45a59ed59ec07028560f6d8e3
Tyllis Xu <LivelyCarpet87@gmail.com>
2020-07-11 11:17:17 +0800

    replaced server.py with cloudant supporting version

--- API/server/server.py
+++ API/server/server.py
@@ -1,36 +1,20 @@
 import flask
 from flask import request, jsonify
 from flask_api import status
-import sqlite3
 import re
 import json
 import hashlib
 import os
+import time
+import atexit
+
+import CustomCloudantModules as ccm
 
 isMacAddr = re.compile(r"([\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2})")
 isFloodAddr = re.compile("FF:FF:FF:FF:FF:FF",re.I)
 
-connPos = sqlite3.connect('Positives.db')
-cursPos = connPos.cursor()
-connSec = sqlite3.connect('Secrets.db')
-cursSec = connSec.cursor()
 
-cursPos.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='positive' ''')
-#if the count is 1, then table exists
-if cursPos.fetchone()[0] != 1 : {
-	cursPos.execute("create table positive (MAC_Addr)")
-}
-connPos.commit()
-cursSec.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='secrets' ''')
-#if the count is 1, then table exists
-if cursSec.fetchone()[0] != 1 : {
-	cursSec.execute("create table secrets (MAC_Addr, Secret_Key)")
-}
-connSec.commit()
-
-this.__baseURL__ = 'http://0.0.0.0:8000/'
-this.__curlHandle__ = pycurl.Curl()
-
+ccm.init()
 
 app = flask.Flask(__name__)
 
@@ -40,226 +24,203 @@ app = flask.Flask(__name__)
 #  Stores a copy of secret in the local database
 @app.route('/InitSelf', methods=["POST"])
 def initSelf():
-    data = request.get_json(force=True)
-    self = data['Self']
-    selfList = parseMacAddr(self)
-    if not selfList:
-        return 'Bad MAC Address!', 400
-    secret = initNewUser(selfList)
-    if secret is not None:
-        return jsonify(
-            Secret = secret
-        ), status.HTTP_201_CREATED
-    else:
-        return 'Already Initiated. ', 400
+	data = request.get_json(force=True)
+	self = data['Self']
+	selfList = parseMacAddr(self)
+	if not selfList:
+		return 'Bad MAC Address!', 400
+	secret = initNewUser(selfList)
+	if secret == "":
+		return 'Already Initiated. ', 400
+	elif secret is None:
+		return status.HTTP_500_INTERNAL_SERVER_ERROR
+	else:
+		return jsonify(
+			Secret = secret
+		), status.HTTP_201_CREATED
+
 
 
 #  Takes in a POST request with a json object containing a SINGLE MAC address and a secret key
 #  Returns a HTTP 201 and a msg = "Get well soon." message in JSON
 @app.route('/positiveReport', methods=["POST"])
 def receivePositiveReport():
-    data = request.get_json(force=True)
-    self = data['Self']
-    secret = data['Secret']
-    metAddrList = data['MetAddrList']
-    addrList = parseMacAddr(self+", "+metAddrList)
-    if not addrList:
-        return 'Bad MAC Address!', 400
-    valid = verifySecret(addrList[0],secret)
-    if valid:
-        markPositive(addrList)
-        return jsonify(
-            msg = "Get well soon. "
-        ), status.HTTP_201_CREATED
-    else:
-        return 'Received', status.HTTP_200_OK
+	data = request.get_json(force=True)
+	self = data['Self']
+	secret = data['Secret']
+	metAddrList = data['MetAddrList']
+	addrList = parseMacAddr(self+", "+metAddrList)
+	if not addrList:
+		return 'Bad MAC Address!', 400
+	valid = verifySecret(addrList[0],secret)
+	if valid:
+		markPositive(addrList, self, secret)
+		return jsonify(
+			msg = "Get well soon. "
+		), status.HTTP_201_CREATED
+	else:
+		return 'Received', status.HTTP_200_OK
 
 
 #  Takes in a POST request string with MAC addresses in CSV format
 #  Returns a Boolean atRisk status in JSON
 @app.route('/QueryMyMacAddr', methods=["POST"])
 def receiveQueryMyMacAddr():
-    data = request.get_json(force=True)
-    self = data['Self']
-    secret = data['Secret']
-    if not verifySecret(self,secret):
-        return 'Bad Request Key', 403
-    addrList = parseMacAddr(self)
-    if not addrList:
-        return 'Bad MAC Address!', 400
-    state = queryAddr(addrList[0])
-    return jsonify(
-        atRisk = state
-    ), status.HTTP_200_OK
+	data = request.get_json(force=True)
+	self = data['Self']
+	secret = data['Secret']
+	if not verifySecret(self,secret):
+		return 'Bad Request Key', 403
+	addrList = parseMacAddr(self)
+	if not addrList:
+		return 'Bad MAC Address!', 400
+	state = queryAddr(addrList[0],secret)
+	if state == 1:
+		return jsonify(
+			 atRisk = True
+			 ), status.HTTP_200_OK
+	elif state == 0:
+		return jsonify(
+				 atRisk = False
+				 ), status.HTTP_200_OK
+	elif state == -1:
+		return 'No such user or invalid keys', 403
+	else:
+		return status.HTTP_500_INTERNAL_SERVER_ERROR
+
 
 
 #  Takes in a POST request with a json object containing a SINGLE MAC address and a secret key
 #  Returns a HTTP 201 and a msg = "Stay healthy." message in JSON
 @app.route('/negativeReport', methods=["POST"])
 def receiveNegativeReport():
-    data = request.get_json(force=True)
-    self = data['Self']
-    secret = data['Secret']
-    addr = parseMacAddr(self)
-    if not addr:
-        return 'Bad MAC Address!', 400
-    valid = verifySecret(addr[0],secret)
-    if valid:
-        markNegative(addr[0])
-        return jsonify(
-            msg = "Stay healthy. "
-        ), status.HTTP_201_CREATED
-    else:
-        return 'Received', status.HTTP_200_OK
+	data = request.get_json(force=True)
+	self = data['Self']
+	secret = data['Secret']
+	addr = parseMacAddr(self)
+	if not addr:
+		return 'Bad MAC Address!', 400
+	valid = verifySecret(addr[0],secret)
+	if valid:
+		markNegative(addr[0], secret)
+		return jsonify(
+			msg = "Stay healthy. "
+		), status.HTTP_201_CREATED
+	else:
+		return 'Received', status.HTTP_200_OK
 
 
 @app.route('/ForgetMe', methods=["POST"])
 def forgetSelf():
-    data = request.get_json(force=True)
-    self = data['Self']
-    secret = data['Secret']
-    addr = parseMacAddr(self)
-    if not addr:
-        return 'Bad MAC Address!', 400
-    valid = verifySecret(addr[0],secret)
-    if valid:
-        deleteUser(addr[0],secret)
-        return jsonify(
-            msg = "Goodbye. "
-        ), status.HTTP_201_CREATED
-    else:
-        return 'Received', status.HTTP_200_OK
+	data = request.get_json(force=True)
+	self = data['Self']
+	secret = data['Secret']
+	addr = parseMacAddr(self)
+	if not addr:
+		return 'Bad MAC Address!', 400
+	valid = verifySecret(addr[0],secret)
+	if valid:
+		deleteUser(addr[0],secret)
+		return jsonify(
+			msg = "Goodbye. "
+		), status.HTTP_201_CREATED
+	else:
+		return 'Received', status.HTTP_200_OK
 
 
 def initNewUser(selfList):
-    connPos = sqlite3.connect('Positives.db')
-    cursPos = connPos.cursor()
-    connSec = sqlite3.connect('Secrets.db')
-    cursSec = connSec.cursor()
-
-    addr = selfList[0]
-    secret = None
-
-	outer = {}
-	inner = {}
-    inner['MAC_Addr'] = addr
-	outer['selector'] = inner
-	data = json.dumps(outer)
-
-	c = this.__curlHandle__
- 	c.setopt(c.URL, this.__baseURL__)
-	c.setopt(c.POSTFIELDS, data)
-    c.setopt(c.WRITEFUNCTION, this.__buffer_obj__.write)
-	c.setopt(c.HTTPHEADER, ['Content-Type: application/json'])
-    c.perform()
-	body = this.__buffer_obj__.getvalue().decode('utf-8')
-	response = json.loads(body)['docs'][0]['age']
-
-
-
-    match = cursSec.fetchone()
-
-    if not match:
-        secret = hashlib.sha224((addr+str(os.urandom(128))).encode('utf-8')).hexdigest()
-        cursSec.execute("insert into secrets values (?, ?)", (addr, secret))
-        connSec.commit()
-
-    connPos.close()
-    connSec.close()
-    return secret
+	addr = selfList[0]
+	secret = ""
+	if not ccm.personExists(addr):
+		secret = hashlib.sha224((addr+str(os.urandom(128))).encode('utf-8')).hexdigest()
+		success = ccm.addPerson(addr,4,secret)  # States: 1. Recovered, 2. Positive, 3. Contacted, 4. Neutral
+		if not success:
+			raise cloudant.error.CloudantDatabaseException
+	else: #person, exists, but may not be initiated. This only occurs if person contacted a person marked positive
+		if ccm.getState(addr) == 3:
+			secret = hashlib.sha224((addr+str(os.urandom(128))).encode('utf-8')).hexdigest()
+			success = cmm.changeSecretKey(addr,secret)
+			if not success:
+				raise cloudant.error.CloudantDatabaseException
+	return secret
 
 
 def verifySecret(addr, secret):
-    connPos = sqlite3.connect('Positives.db')
-    cursPos = connPos.cursor()
-    connSec = sqlite3.connect('Secrets.db')
-    cursSec = connSec.cursor()
-
-    safetyCheck = re.compile(r'^([a-z0-9]{56})$')
-    try:
-        safeSecret = safetyCheck.fullmatch(str(secret)).group(1)
-    except AttributeError:
-        return False
-    if not safeSecret:
-        return False
-
-    cursSec.execute("select * from secrets where MAC_Addr=:MAC_Addr and Secret_Key=:Secret_Key", {"MAC_Addr": addr, "Secret_Key": secret})
-    match = cursSec.fetchone()
-
-    connPos.close()
-    connSec.close()
-    if match is None:
-        return False
-    else:
-        return True
+	safetyCheck = re.compile(r'^([a-z0-9]{56})$')
+	try:
+		safeSecret = safetyCheck.fullmatch(str(secret)).group(1)
+	except AttributeError:
+		return False
+	if not ccm.personExists(addr):
+		return False
+	if not safeSecret:
+		return False
+	if secret == ccm.getSecretKey(addr):
+		return True
+	else:
+		return False
 
 
-def markPositive(addrList):
-    connPos = sqlite3.connect('Positives.db')
-    cursPos = connPos.cursor()
-    connSec = sqlite3.connect('Secrets.db')
-    cursSec = connSec.cursor()
-    for positive in addrList:
-        cursPos.execute("select * from positive where MAC_Addr=:MAC_Addr", {"MAC_Addr": positive,})
-        match = cursPos.fetchone()
-        if not match:
-            cursPos.execute("insert into positive values (?)", (positive, ))
-            connPos.commit()
-    connPos.close()
-    connSec.close()
+def markPositive(addrList, self, secret):
+	if not verifySecret(self,secret):  # Do nothing if secret key does not match
+		return None
+	for positive in addrList:
+		if ccm.personExists(positive):  # Change state if person exists
+			# retry the write to the database up to 10 times if it fails
+			attempt = 1
+			while attempt <= 10:
+				success = ccm.changeState(positive,3)
+				time.sleep(1)  # Delay to prevent reaching free tier IBM Cloudant limits
+				if success:
+					break
+				else:
+					attempt = attempt + 1
+		else:
+			# if person not exist, create an unintiated Person with state
+			attempt = 1
+			while attempt <= 10:
+				success = ccm.addPerson(positive,3,"")
+				time.sleep(1)  # Delay to prevent reaching free tier IBM Cloudant limits
+				if success:
+					break
+				else:
+					attempt = attempt + 1
 
 
-def markNegative(negative):
-    connPos = sqlite3.connect('Positives.db')
-    cursPos = connPos.cursor()
-    connSec = sqlite3.connect('Secrets.db')
-    cursSec = connSec.cursor()
-
-    cursPos.execute("delete from positive where MAC_Addr=:MAC_Addr",  {"MAC_Addr": negative,})
-    connPos.commit()
-
-    connPos.close()
-    connSec.close()
+def markNegative(negative,secret):
+	if not verifySecret(negative,secret):  # Do nothing if secret key does not match
+		return None
+	ccm.changeState(negative,1)  # Mark person as recovered
 
 
 def deleteUser(user, secret):
-    connPos = sqlite3.connect('Positives.db')
-    cursPos = connPos.cursor()
-    connSec = sqlite3.connect('Secrets.db')
-    cursSec = connSec.cursor()
-
-    cursPos.execute("delete from positive where MAC_Addr=:MAC_Addr",  {"MAC_Addr": user,})
-    connPos.commit()
-    cursSec.execute("delete from secrets where MAC_Addr=:MAC_Addr and Secret_Key=:Secret_Key",  {"MAC_Addr": user,"Secret_Key": secret})
-    connSec.commit()
-
-    connPos.close()
-    connSec.close()
+	if not verifySecret(user,secret):  # Do nothing if secret key does not match
+		return None
+	ccm.removePerson(user)
 
 
-def queryAddr(addr):
-    connPos = sqlite3.connect('Positives.db')
-    cursPos = connPos.cursor()
-    connSec = sqlite3.connect('Secrets.db')
-    cursSec = connSec.cursor()
-
-
-    cursPos.execute("select * from positive where MAC_Addr=:addr", {"addr": addr})
-    match = cursPos.fetchone()
-    connPos.close()
-    connSec.close()
-    if match is not None:
-        return True
-    else:
-        return False
+def queryAddr(addr, secret):
+	if not verifySecret(addr,secret):  # Do nothing if secret key does not match
+		return -1  # return 403 forbidden incorrect secret or user not exist
+	if ccm.getState(addr) == 3:
+		return 1
+	return 0
 
 
 def parseMacAddr(AddrStr):
-    #sanitization of all input
-    addrList = re.findall(isMacAddr,AddrStr)
-    for addr in addrList:
-        if re.match(isFloodAddr,addr) is not None:
-            addrList.remove(addr)
-    return addrList
+	#sanitization of all input
+	addrList = re.findall(isMacAddr,AddrStr)
+	for addr in addrList:
+		if re.match(isFloodAddr,addr) is not None:
+			addrList.remove(addr)
+	return addrList
 
-#app.run()
+
+@atexit.register
+def shutdown():
+	ccm.cloudantCleanup()
+
+
+port = int(os.getenv('PORT', 8000))
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=port, debug=False)
