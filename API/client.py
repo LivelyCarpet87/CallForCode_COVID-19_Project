b9439afbcab0f57b46793950b7150f4d920adefa
Tyllis Xu <LivelyCarpet87@gmail.com>
2020-07-11 11:18:24 +0800

    Update client.py

--- API/client.py
+++ API/client.py
@@ -10,7 +10,8 @@ except ImportError:
 
 this = sys.modules[__name__]
 this.__buffer_obj__ = BytesIO()
-this.__baseURL__ = 'http://0.0.0.0:8000/'
+this.__baseURL__ = 'https://covidcontacttracerapp-smart-zebra-ua.mybluemix.net/'
+#this.__baseURL__ = 'http://0.0.0.0:8000/'
 this.__curlHandle__ = pycurl.Curl()
 
 def initSelf(MacAddrSelf):
@@ -29,18 +30,22 @@ def initSelf(MacAddrSelf):
 
     code = c.getinfo(pycurl.HTTP_CODE)
     body = this.__buffer_obj__.getvalue().decode('utf-8')
-
     resetResources()
-    print(body)
-    secretPattern = re.compile(r'{"Secret":"(\S{56})"}')
-    secret = secretPattern.match(body).group(1)
-    print(secret)
-
+    if "Initiated." not in body and code == 201:
+        try:
+            secretPattern = re.compile(r'(\S{56})')
+            jdata = json.loads(body)
+            secret = jdata['Secret']
+            secret = secretPattern.match(secret).group(1)  # sanitization
+        except KeyError:
+            return 1
     if code == 201:
         return secret
     elif code != 200:
         #  Retry
         return 2
+    elif code == 400:
+        return 3  # Permission denied due to initiated
     else:
         #  Unknown Error
         return 1
@@ -140,6 +145,37 @@ def queryMyMacAddr(self,secret):
         return 0
 
 
+def forgetUser(MacAddrSelf, secretKey):
+    c = this.__curlHandle__
+    c.setopt(c.URL, this.__baseURL__+'ForgetMe')
+    d = {}
+    d['Self'] = MacAddrSelf
+    d['Secret'] = secretKey
+    # Form data must be provided already urlencoded.
+    postfields = json.dumps(d)
+    # Sets request method to POST,
+    # Content-Type header to application/x-www-form-urlencoded
+    # and data to send in request body.
+    c.setopt(c.POSTFIELDS, postfields)
+    c.setopt(c.WRITEFUNCTION, this.__buffer_obj__.write)
+    c.perform()
+
+    code = c.getinfo(pycurl.HTTP_CODE)
+    body = str(this.__buffer_obj__.getvalue())
+    print(body)
+    resetResources()
+
+    if code == 201 and "Goodbye. " in body:
+        #  Server Ack Success
+        return 0
+    elif code == 200:
+        #  Retry
+        return 2
+    else:
+        #  Unknown Issue Occurred
+        return 1
+
+
 def resetResources():
     c = this.__curlHandle__
     del this.__buffer_obj__
@@ -153,13 +189,17 @@ def freeResources():
 def tests():
     self = "FF:11:2E:7A:5B:6A"
     others = "4F:11:2E:7A:5B:6A, 4F:1A:2E:7A:5B:6A, 4F:11:77:7A:5B:6A"
-    secret = initSelf(self)
-    print(secret)
-    print(queryMyMacAddr(self,secret))
-    print(positiveReport(self,secret,others))
-    print(queryMyMacAddr(self,secret))
-    print(negativeReport(self,secret))
-    print(queryMyMacAddr(self,secret))
+    person2 = "4F:11:2E:7A:5B:6A"
+    secret1 = initSelf(self)
+    secret2 = initSelf(person2)
+    print(len(secret1)==56)
+    print(queryMyMacAddr(self,secret1)==0)
+    print(positiveReport(self,secret1,others)==0)
+    print(queryMyMacAddr(person2,secret2)==0)
+    print(negativeReport(self,secret1)==0)
+    print(queryMyMacAddr(self,secret1)==0)
+    print(forgetUser(self, secret1)==0)
+    print(forgetUser(person2, secret2)==0)
     freeResources()
 
 tests()
