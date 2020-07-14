26b54be7cb58b980cfeb86df991132e62776833b
Tyllis Xu <LivelyCarpet87@gmail.com>
2020-07-14 19:07:17 +0800

    Updated MAC Address extraction, pending final decision

--- Kivy/CallForCodeV2.py
+++ Kivy/CallForCodeV2.py
@@ -13,6 +13,9 @@ import subprocess
 #TIME
 import datetime
 
+#Regular Expressions
+import re
+
 
 
 class storageUnit():
@@ -22,7 +25,7 @@ class storageUnit():
         self.macDict = dict() #Key corresponding to list
         self.recentTen = []
         self.prevNetwork = set()
-    
+
     def addEntry(self, macAddress, time):
         if macAddress in self.macDict:
             self.macDict[macAddress] += [time]
@@ -33,41 +36,41 @@ class storageUnit():
             self.macDict[macAddress] = [time]
             self.recentTen = [[time, macAddress]] + self.recentTen[:9]
             self.prevNetwork.add(macAddress)
-        
-            
+
+
 
 class MyGrid(GridLayout):
     def __init__(self, **kwargs):
         super(MyGrid, self).__init__(**kwargs)
-        
-        self.storage = storageUnit() 
-        
+
+        self.storage = storageUnit()
+
         self.cols = 1
-        
+
         self.smallGrid = GridLayout()
         self.smallGrid.cols = 1
-        
+
         self.label1 = Label(text = "Name : Ryan")
         self.smallGrid.add_widget(self.label1)
-        
+
         self.label2 = Label(text = "SelfMac : placeHolder")
         self.smallGrid.add_widget(self.label2)
-        
-        
+
+
         self.submit = Button(text = "Submit", font_size = 40)
         self.submit.bind(on_press = self.pressed)
         self.smallGrid.add_widget(self.submit)
-        
+
         self.add_widget(self.smallGrid)
-        
+
         self.label3 = Label(text = "foundMac : placeHolder")
         self.add_widget(self.label3)
-        
+
     def pressed(self, instance):
         print("button pressed")
         macList = self.getMac()
         self.label3.text = "SelfMac : " + macList
-        
+
     def getString(self, recentTen):
         returnStr = ""
         for i in recentTen:
@@ -77,13 +80,17 @@ class MyGrid(GridLayout):
         result = subprocess.run(['arp', '-a'], stdout=subprocess.PIPE)
 
         macInitStr = result.stdout
-        
+
         macInitStr = repr(macInitStr)
+        isMacAddr = re.compile(r"([\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2}:[\da-f|A-F]{2})")
+        macList = re.findall(isMacAddr,macInitStr)
+        """
         splitArr = macInitStr.split(" ")
         macList = []
         for part in splitArr:
             if part.count(":") == 5:
                 macList += [part]
+        """
         compareSet = set(macList)
         print("compareSet = " + repr(compareSet))
         print("prevNetwork = " + repr(self.storage.prevNetwork))
@@ -94,7 +101,7 @@ class MyGrid(GridLayout):
             for macAdd in diff:
                 self.storage.addEntry(macAdd, datetime.datetime.now())
             return self.getString(self.storage.recentTen)
-        
+
 
 class MyApp(App):
     def build(self):
