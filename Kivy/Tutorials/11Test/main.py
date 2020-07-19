f516b73b91987c0ef1ff107831d1dd963be1a63b
RyanTooOp <42991620+RyanTooOp@users.noreply.github.com>
2020-07-20 00:43:29 +0800

    SeeData and Button Messages added

--- Kivy/Tutorials/11Test/main.py
+++ Kivy/Tutorials/11Test/main.py
@@ -4,6 +4,8 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
 from kivy.properties import ObjectProperty
 from kivy.graphics import Rectangle
 from kivy.graphics import Color
@@ -27,7 +29,7 @@ import datetime
 #Regular Expressions
 import re
 
-
+#Manages all permanent storage and adding into the JSON file
 class storageUnit():
 
     def __init__(self):
@@ -43,7 +45,7 @@ class storageUnit():
         print(self.store.get("prevNetwork")["value"])
         print("AFTER PRINT _________")
         
-
+#Adds a unknown / new mac address that was not on the previous network into the json file
     def addEntry(self, macAddress, time):
         if macAddress in self.store.get("macDict")["value"]:
             self.store.get("macDict")["value"][macAddress] += [time]#HEREEE
@@ -54,6 +56,7 @@ class storageUnit():
             self.store.get("macDict")["value"][macAddress] = [time]
             self.store.get("recentTen")["value"] = [[time, macAddress]] + self.store.get("recentTen")["value"][:9]
             #self.store.get("prevNetwork")["value"][macAddress] = 0
+#Checks if the previous prevNetwork is the same as foreignSet, which is a set
     def isSamePrevNetwork(self, foreignSet):
         returnArr = []
         for i in foreignSet:
@@ -61,6 +64,7 @@ class storageUnit():
                 returnArr += [i]
         return returnArr
 
+#This entire class is meant for macAddress collection
 class GetMacAdd():
     def __init__(self, **kwargs):
         #super(HomePage, self).__init__(**kwargs)
@@ -148,11 +152,14 @@ class GetMacAdd():
             self.storage.store.put("prevNetwork", value = dict.fromkeys(compareSet, 0))
             return self.getString(self.storage.store.get("recentTen")["value"])
 
+#Class for the homepage screen
 class HomePage(Screen, Widget):
     def __init__(self, **kwargs):
         super(HomePage, self).__init__(**kwargs)
+#References the json file called local.json
         self.store = JsonStore('local.json')
         print("whether previous exist = " + repr(self.store.exists('numEntries')))
+#Checks if there is a file. If there is not, initiate all 4 necessary parts
         if (not self.store.exists('numEntries')):
             print("enter")
             self.store.put("numEntries", value = 0)
@@ -160,12 +167,13 @@ class HomePage(Screen, Widget):
             self.store.put("recentTen", value = list())
             self.store.put("prevNetwork", value = dict())
         self.options = ObjectProperty(None)
+#macClass variable is just used as a reference to be able to call the getMac class
         self.macClass = GetMacAdd()
         self.actualMac = self.macClass.getMac()
         
     
     
-    
+#This method is used when we click the button to check our current network mac
     def calculateMac(self):
         self.actualMac = self.macClass.getMac()
         return self.actualMac
@@ -182,21 +190,61 @@ class HomePage(Screen, Widget):
     
     pass
 
+#SideBar class page (reference my.kv file)
 class SideBarPage(Screen):
     pass
 
-class AboutUsPage(Screen):
-    
-            
+#AboutUs class page (reference my.kv file)
+class AboutUsPage(Screen):      
     pass
 
+#QuitApp class page (reference my.kv file)
 class QuitAppPage(Screen):
+    def deleteDataButtonClicked(self):
+        print("DeleteData button Clicked")
+    def quitButtonClicked(self):
+        print("Quit button clicked")
     pass
 
+#SendData class page (reference my.kv file)
 class SendDataPage(Screen):
+    def sendDataButtonClicked(self):
+        print("sendData button clicked")
     pass
 
+#SeeDataPage class page (reference my.kv file)
 class SeeDataPage(Screen):
+    def __init__(self, **kwargs):
+        super(SeeDataPage, self).__init__(**kwargs)
+        print("ENTER SEEDATAPAGE INIT")
+        self.store = JsonStore('local.json')
+#Used for future reference and changing the data in the table
+        self.data = [0] * 20
+#Stores the recentTen aspect of the json file
+        self.recentTen = self.store.get("recentTen")["value"]
+#Creates the grid used to display the information
+        self.table = GridLayout()
+        self.table.cols = 2
+        
+        
+        print("BEFORE ASSIGN VALUES")
+#Initiates the table by first creating a label into the self.data array, and 
+#then adding them to the grid
+        for i in range(len(self.recentTen)):
+            self.data[2 * i] = Label(text = self.recentTen[i][1])
+            self.data[2 * i + 1] = Label(text = self.recentTen[i][0])
+            self.table.add_widget(self.data[2 * i])
+            self.table.add_widget(self.data[2 * i + 1])
+        self.add_widget(self.table)
+
+#This method changes the self.data so that it reflects the new recentTen
+    def renewRecentTen(self):
+        print("renew clicked")
+        self.recentTen = self.store.get("recentTen")["value"]
+        for i in range(len(self.recentTen)):
+            self.data[2 * i].text = self.recentTen[i][1]
+            self.data[2 * i + 1].text = self.recentTen[i][0]
+
     pass
 
 #Represent the transitions between the windows above
