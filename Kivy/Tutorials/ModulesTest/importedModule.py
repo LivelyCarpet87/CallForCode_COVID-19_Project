8f52691aa2f18ba84a338ac00449254bb98ebf12
RyanTooOp <42991620+RyanTooOp@users.noreply.github.com>
2020-07-21 20:53:59 +0800

    CommitTemporary
    Committing to pull

---
+++ Kivy/Tutorials/ModulesTest/importedModule.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 15:43:19 2020
+
+@author: ryan04px2021
+"""
+
+def tryFunction():
+    return "hello"
---
+++ Kivy/Tutorials/ModulesTest/importingModule.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 21 15:43:01 2020
+
+@author: ryan04px2021
+"""
+
+from importedModule import *
+print(importedModule.tryFunction())
